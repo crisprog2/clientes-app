d94ebb325335c54ca9d80d82d91672cdd643a73e
christian chacon <christian_kazuma@hotmail.com>
2022-02-02 16:47:01 -0500

    Se implementan las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientesComponent } from './clientes/clientes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/clientes', pathMatch: 'full'},
+  {path: 'clientes', component: ClientesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
