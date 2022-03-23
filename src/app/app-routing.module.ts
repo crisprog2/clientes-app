c7ecfcd4ef87d513c17a3b400ee8685093931663
Christian <christian_kazuma@hotmail.com>
2022-03-23 09:05:07 -0500

    156. Añadiendo seguridad en las rutas con Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientesComponent } from './clientes/clientes.component';
 import { FormComponent } from './clientes/form.component';
+import { AuthGuard } from './usuarios/guards/auth.guard';
 import { LoginComponent } from './usuarios/login.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/clientes', pathMatch: 'full'},
   {path: 'clientes', component: ClientesComponent},
   {path: 'clientes/page/:page', component: ClientesComponent},
-  {path: 'clientes/form', component: FormComponent},
-  {path: 'clientes/form/:id', component: FormComponent},
+  {path: 'clientes/form', component: FormComponent, canActivate:[AuthGuard]},
+  {path: 'clientes/form/:id', component: FormComponent, canActivate:[AuthGuard]},
   {path: 'login', component: LoginComponent}
 ];
 
